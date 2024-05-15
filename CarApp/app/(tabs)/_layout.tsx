540cf2e602cf93216e81c1d00c17ad5c679f1b0a
Dimaldinho <dlepp@inbox.lv>
2024-05-15 19:06:18 +0300

    New Button for loggout

--- CarApp/app/(tabs)/_layout.tsx
+++ CarApp/app/(tabs)/_layout.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { Link, Tabs } from 'expo-router';
-import { Pressable } from 'react-native';
+import { Pressable,View } from 'react-native';
 import Colors from '@/constants/Colors';
 import { useColorScheme } from '@/components/useColorScheme';
 import { useClientOnlyValue } from '@/components/useClientOnlyValue';
-
+import { StyleSheet } from 'react-native';
 // You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
 function TabBarIcon(props: {
   name: React.ComponentProps<typeof FontAwesome>['name'];
@@ -31,6 +31,7 @@ export default function TabLayout() {
           title: 'Tab One',
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
           headerRight: () => (
+            <View style={styles.headerButtonsContainer}>
             <Link href="/modal" asChild>
               <Pressable>
                 {({ pressed }) => (
@@ -43,6 +44,19 @@ export default function TabLayout() {
                 )}
               </Pressable>
             </Link>
+            <Link href="/modal" asChild>
+              <Pressable>
+                {({ pressed }) => (
+                  <FontAwesome
+                    name="home"
+                    size={25}
+                    color={Colors[colorScheme ?? 'light'].text}
+                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+                  />
+                )}
+              </Pressable>
+            </Link>
+            </View>
           ),
         }}
       />
@@ -56,3 +70,9 @@ export default function TabLayout() {
     </Tabs>
   );
 }
+const styles = StyleSheet.create({
+  headerButtonsContainer: {
+    flexDirection: 'row', // Horizontal arrangement
+    marginRight: 10, // Adjust as needed for spacing between buttons
+  },
+});
