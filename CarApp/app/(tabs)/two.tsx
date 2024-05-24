8ac45ac4e50f0c4bc7329766775a2318cce492d1
Dimaldinho <dlepp@inbox.lv>
2024-05-24 10:57:20 +0300

    feat(Tab two): Now data about repairs are shown in tab two

--- CarApp/app/(tabs)/two.tsx
+++ CarApp/app/(tabs)/two.tsx
@@ -1,16 +1,65 @@
-import { Pressable, StyleSheet } from 'react-native';
+import { Pressable, ScrollView, StyleSheet } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
 import { router } from 'expo-router';
+import getCarRepairData from '../../scripts/getRepairInfo'
 
+import React, { useEffect, useState } from "react";
+
+   
+   
 export default function TabTwoScreen() {
+  const [data, setData] = useState([])
+  const [refresh, setRefresh] = useState(false); // State to trigger useEffect
+  
+  const showAll = async ()=>{
+      const data = await getCarRepairData()
+      console.log(data)
+      setData(data);
+      
+  }
+  const handlePress = () => {
+    setRefresh(!refresh);
+    router.push('../carRepairInfoInput');
+  };
+  useEffect(() => {
+    
+    showAll()
+    
+  
+    
+  }, [refresh]);
+
   return (
     <View style={styles.container}>
-      <Pressable onPress={()=> router.push('../carRepairInfoInput')} style={styles.button}>
-            <Text>Add</Text>
-      </Pressable>
+      <ScrollView contentContainerStyle={styles.container}>
+        {data.map((item) => (
+          <View key={item.id} style={styles.item}>
+            <Text style={styles.itemText}>{item.info} - {item.price}$</Text>
+            <Text style={styles.itemText}>{item.date}</Text>
+            
+          </View>
+          
+        ))}
+        
+      </ScrollView>
+      <Pressable onPress={handlePress} style={styles.button}>
+              <Text>Add</Text>
+              
+        </Pressable>
     </View>
+
+
+
+    // <View style={styles.container}>
+      //  <Pressable onPress={()=> router.push('../carRepairInfoInput')} style={styles.button}>
+      //        <Text>Add</Text>
+            
+      //  </Pressable>
+      
+    // </View>
+    
   );
 }
 
@@ -19,6 +68,7 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    
   },
   title: {
     fontSize: 20,
@@ -29,9 +79,26 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },button: {
-    backgroundColor: '#007BFF',
-    padding: 10,
+    position: 'absolute',
+    bottom: 20,
+    left: '50%',
+    marginLeft: -75, // Adjust this value to half of the button's width
+    backgroundColor: '#007bff',
+    paddingVertical: 15,
+    paddingHorizontal: 30,
     borderRadius: 5,
-    bottom: -350,
+    alignItems: 'center',
+  }, item: {
+    padding: 20,
+    marginVertical: 10,
+    backgroundColor: '#f9c2ff',
+    borderRadius: 5,
+    width: '100%',
+    alignItems: 'center',
+  },itemText: {
+    fontSize: 18,
+  },scrollViewContent: {
+    padding: 20,
+    paddingBottom: 80, // Add some padding to avoid content being hidden behind the button
   },
 });
